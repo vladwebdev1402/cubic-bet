9c1dabe07454598740a21840f4cbe0d8d170980b
vladwebdev1402 <mmovlad.kriven.1402@gmail.com>
2024-07-10 04:17:22 +0700

    add config for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -9,4 +9,5 @@ export default defineConfig({
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, './src/') }],
   },
+  base: 'https://vladwebdev1402.github.io/cubic-bet/',
 });
