0762be6ba6241a7b5481c33cc65772adb0dc4dd6
vladwebdev1402 <mmovlad.kriven.1402@gmail.com>
2024-07-09 18:39:43 +0700

    add Button

---
+++ src/components/atoms/Button/Button.tsx
@@ -0,0 +1,50 @@
+import { ComponentProps, FC, MouseEvent, ReactNode } from 'react';
+import clsx from 'clsx';
+
+import style from './Button.module.scss';
+
+type ButtonProps = {
+  theme?: 'violet' | 'green';
+  lastIcon?: ReactNode;
+  isActive?: boolean;
+  loading?: boolean;
+} & ComponentProps<'button'>;
+
+const Button: FC<ButtonProps> = ({
+  theme = 'violet',
+  className = '',
+  loading = false,
+  isActive = false,
+  lastIcon,
+  children,
+  disabled,
+  ...props
+}) => {
+  const onIconClick = (e: MouseEvent<HTMLDivElement>) => {
+    e.stopPropagation();
+  };
+
+  return (
+    <button
+      {...props}
+      disabled={disabled || loading}
+      className={clsx(
+        style.button,
+        style[theme],
+        {
+          [style.active]: isActive,
+        },
+        className,
+      )}
+    >
+      {loading && <span className={style.loader} />}
+      {!loading && (
+        <>
+          {children} {lastIcon && <div onClick={onIconClick}>{lastIcon}</div>}
+        </>
+      )}
+    </button>
+  );
+};
+
+export { Button };
--- src/components/pages/MainPage/MainPage.tsx
+++ src/components/pages/MainPage/MainPage.tsx
@@ -1,13 +1,29 @@
-import { Typography } from '@/components/atoms';
+import { Button } from '@/components/atoms';
 
 import style from './MainPage.module.scss';
 
 const MainPage = () => {
   return (
     <div className={style.page}>
-      <Typography>paragraph_16</Typography>
-      <Typography variant="paragraph_14">paragraph_14</Typography>
-      <Typography variant="title">title</Typography>
+      <Button>asasdads</Button>
+      <Button theme="green">asasdads</Button>
+      <Button isActive>asasdads</Button>
+      <Button disabled>disabled</Button>
+      <Button loading>disabled</Button>
+      <Button
+        className={style.btn}
+        lastIcon={
+          <div
+            style={{
+              width: '16px',
+              height: '16px',
+              backgroundColor: 'white',
+            }}
+          />
+        }
+      >
+        Конкретное ч
+      </Button>
     </div>
   );
 };
